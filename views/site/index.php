4b1d38f6101495e145d976a14fbe50719c39b596
Георгий Егоров <georgmix300@mail.ru>
2021-08-14 23:56:51 +0400

    footer

--- views/site/index.php
+++ views/site/index.php
@@ -69,16 +69,3 @@ $this->title = 'Blog';
         ?>
     </div>
 </div>
-<!---->
-<!--<div class="row tm-mb-90">-->
-<!--    <div class="col-12 d-flex justify-content-between align-items-center tm-paging-col">-->
-<!--        <a href="javascript:void(0);" class="btn btn-primary tm-btn-prev mb-2 disabled">Previous</a>-->
-<!--        <div class="tm-paging d-flex">-->
-<!--            <a href="javascript:void(0);" class="active tm-paging-link">1</a>-->
-<!--            <a href="javascript:void(0);" class="tm-paging-link">2</a>-->
-<!--            <a href="javascript:void(0);" class="tm-paging-link">3</a>-->
-<!--            <a href="javascript:void(0);" class="tm-paging-link">4</a>-->
-<!--        </div>-->
-<!--        <a href="javascript:void(0);" class="btn btn-primary tm-btn-next">Next Page</a>-->
-<!--    </div>-->
-<!--</div>-->
